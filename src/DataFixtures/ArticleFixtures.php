533b01c1cd9396a1ba310d59b5b0a31c537201de
Thomas POULARD <pro.thomaspoulard@gmail.com>
2021-11-09 17:19:27 +0100

    added 100 entries in database, generated randomly with faker

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,19 +5,23 @@ namespace App\DataFixtures;
 use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class ArticleFixtures extends Fixture
 {
+    
     public function load(ObjectManager $manager): void
     {
-        /* for ($i = 0; $i < 100; $i++) {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < 100; $i++) {
             $article = new Article();
-            $article->setTitle("l'article cool");
-            $article->setContent("le content");
-            $article->setDate(new \DateTime('now'));
+            $article->setTitle($faker->sentence());
+            $article->setContent ($faker->paragraph(3, true));
+            $article->setDate($faker->dateTime());
     
             $manager->persist($article);
-        }*/
+        }
 
         $manager->flush();
     }
